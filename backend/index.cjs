a12d219065f3e9360048569c00f834f46e6eff6a
Satakshi Garg <gargsatakshi54@gmail.com>
2025-10-05 18:04:41 +1100

    added

--- backend/index.cjs
+++ backend/index.cjs
@@ -17,7 +17,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type'],
   maxAge: 86400,
 }));
-app.options('*', cors());
+app.options('/(.*)', cors());
 
 app.use(express.json());
 app.use('/orders', orderRoutes);

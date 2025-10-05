affaaf23dac4deba0f30ebd235572f7ed327fc11
Satakshi24 <64140374+Satakshi24@users.noreply.github.com>
2025-10-05 19:50:38 +1100

    Update orders.cjs

--- backend/routes/orders.cjs
+++ backend/routes/orders.cjs
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const prisma = require('../db');
+const prisma = require('../db.cjs');
 const { cacheGet, cacheSet, cacheDelByPrefix } = require('../redis.cjs');
 
 /**
